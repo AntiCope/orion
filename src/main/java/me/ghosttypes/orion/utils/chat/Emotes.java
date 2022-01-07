a80d0791d47f86244b1233070c108b6c06b0f336
C10udburst <18114966+C10udburst@users.noreply.github.com>
2022-01-07 15:00:46 +0100

    emotes

--- src/main/java/me/ghosttypes/orion/utils/chat/Emotes.java
+++ src/main/java/me/ghosttypes/orion/utils/chat/Emotes.java
@@ -12,6 +12,14 @@ public class Emotes {
         if (msg.contains(":pick:")) msg = msg.replace(":pick:", "⛏");
         if (msg.contains(":wheelchair:")) msg = msg.replace(":wheelchair:", "♿");
         if (msg.contains(":lightning:")) msg = msg.replace(":lightning:", "⚡");
+        if (msg.contains(":rod:")) msg = msg.replace(":rod:", "🎣");
+        if (msg.contains(":potion:")) msg = msg.replace(":potion:", "🧪");
+        if (msg.contains(":fire:")) msg = msg.replace(":fire:", "🔥");
+        if (msg.contains(":shears:")) msg = msg.replace(":shears:", "✂");
+        if (msg.contains(":bell:")) msg = msg.replace(":bell:", "🔔");
+        if (msg.contains(":bow:")) msg = msg.replace(":bow:", "🏹");
+        if (msg.contains(":trident:")) msg = msg.replace(":trident:", "🔱");
+        if (msg.contains(":cloud:")) msg = msg.replace(":cloud:", "☁");
         return msg;
     }
 
